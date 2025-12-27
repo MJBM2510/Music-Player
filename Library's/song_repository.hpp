2f740da09dfb87b82ef84cc9079838e1a7195cee
MJ <mohamnarut@gmail.com>
2025-12-27 20:38:54 +0330

    class SongRepository definded + setSong removed from Song class and pointer's intialization added

--- Library's/song_repository.hpp
+++ Library's/song_repository.hpp
@@ -2,6 +2,7 @@
 #include <string>
 
 class Song{
+    friend class SongRepository;
     private:
         std::string name, artist;
         unsigned int year;
@@ -12,17 +13,74 @@ class Song{
     
     public:
         Song(std::string name, std::string artist,
-            unsigned int year, std::string id = "", unsigned int duration = 0) : 
-                name(name), artist(artist), year(year), id(id), duration(duration) {}
+            unsigned int year, std::string id, unsigned int duration = 0) : 
+                name(name), artist(artist), year(year), id(id), duration(duration), nextSong(nullptr), previousSong(nullptr) {}
+};
 
-        void setSong(std::string name, std::string artist,
-            unsigned int year, std::string id = "", unsigned int duration = 0){
-                this->name = name;
-                this->artist = artist;
-                this->year = year;
-                this->id = id;
-                this->duration = duration;
-                nextSong = nullptr;
-                previousSong = nullptr;
+class SongRepository{
+    private:
+        Song* head;
+        Song* tail;
+    
+    public:
+        SongRepository() : head(nullptr), tail(nullptr) {}
+
+        void addSong(std::string name, std::string artist,
+            unsigned int year, std::string id, unsigned int duration = 0){
+                Song* newSong = new Song(name, artist, year, id, duration);
+
+                if(head == nullptr){
+                    head = newSong;
+                    tail = newSong;
+                }
+                else{
+                    tail->nextSong = newSong;
+                    newSong->previousSong = tail;
+                    tail = newSong;
+                }
+        }
+
+        void deleteSong(const std::string& id){
+            for(auto* currentSong = head; currentSong != nullptr; currentSong = currentSong->nextSong){
+                if(currentSong->id == id){
+                    if(currentSong == head) head = currentSong->nextSong;
+                    else currentSong->previousSong->nextSong = currentSong->nextSong;
+
+                    if(currentSong == tail) tail = currentSong->previousSong;
+                    else currentSong->nextSong->previousSong = currentSong->previousSong;
+
+                    auto name   = currentSong->name;
+                    auto artist = currentSong->artist;
+                    auto sid    = currentSong->id;
+
+                    delete currentSong;
+
+                    std::cout<<"Song "<<name<<" by "<<artist<<", id: "<<sid<<" has deleted successfully\n";
+
+                    return;
+                }
             }
+
+            std::cout<<"NO song find with id: "<<id<<std::endl;
+        }
+
+        void searchSong(const std::string& keyword){
+            for(auto* currentSong = head; currentSong != nullptr; currentSong = currentSong->nextSong){
+                if((currentSong->artist == keyword)|| (currentSong->name == keyword)){
+                    std::cout<<"Title: "<<currentSong->name<<", Artist: "<<currentSong->artist<<std::endl;
+                    return;
+                }
+            }
+
+            std::cout<<"No song/artist fine with similar keyword: "<<keyword<<std::endl;
+        }
+
+        ~SongRepository() {
+            while (head) {
+                Song* temp = head;
+                head = head->nextSong;
+                delete temp;
+            }
+            tail = nullptr;
+        }
 };
