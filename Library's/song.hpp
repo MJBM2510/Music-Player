ffec151418a22c81b7579000a7bf30e09105f43f
MJ <mohamnarut@gmail.com>
2026-01-02 15:54:26 +0330

    song class defined

---
+++ Library's/song.hpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+class Song{
+    friend class SongRepository;
+    private:
+        std::string name, artist;
+        unsigned int year;
+        std::string id;
+        unsigned int duration;
+        Song* nextSong;
+        Song* previousSong;
+    
+    public:
+        Song(std::string name, std::string artist,
+            unsigned int year, std::string id, unsigned int duration = 0) : 
+                name(name), artist(artist), year(year), id(id), duration(duration), nextSong(nullptr), previousSong(nullptr) {}
+};
